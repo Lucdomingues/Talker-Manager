a0c275f4f2b82720454a6e6a693f1ba5993a28bc
Lucdomingues <lojalucc@outlook.com>
2023-02-15 00:20:08 -0300

    [feat]created:validation_minddlewares_endpoint_get(/talker/search?q=searchTerm)in_talkRoutes.js_middlewares_in_validateTalk.js

--- src/routes/talkRoutes.js
+++ src/routes/talkRoutes.js
@@ -12,13 +12,25 @@ const path = 'src/talker.json';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
     try {
         const result = await readFs();
         if (result.length === 0) {
             return res.status(200).json([]);
         }
         return res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/search', validateToken, async (req, res) => {
+    try {
+        const { q } = req.query;
+        const readFile = await readFs();
+        const searchQuery = readFile.filter((element) => element.name.includes(q));
+        return res.status(200).json(searchQuery);
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: error.message });
@@ -70,23 +82,33 @@ router.put('/:id', validateToken,
     validateTalk,
     validateRate,
     async (req, res) => {
-    const { id } = req.params;
-    const { name, age, talk } = req.body;
-    const readFile = await readFs();
-    const index = readFile.findIndex((element) => element.id === Number(id));
-    const updateTalker = { id: Number(id), name, age, talk };
-    readFile[index] = updateTalker;
-    await writeFs(path, JSON.stringify([...readFile]));
-    return res.status(200).json(readFile[index]);
+        try {
+            const { id } = req.params;
+            const { name, age, talk } = req.body;
+            const readFile = await readFs();
+            const index = readFile.findIndex((element) => element.id === Number(id));
+            const updateTalker = { id: Number(id), name, age, talk };
+            readFile[index] = updateTalker;
+            await writeFs(path, JSON.stringify([...readFile]));
+            return res.status(200).json(readFile[index]);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: error.message });
+        }
     });
  
 router.delete('/:id', validateToken, async (req, res) => {
-    const { id } = req.params;
-    const readFile = await readFs();
-    const index = readFile.findIndex((element) => element.id === Number(id));
-    readFile.splice(index, 1);
-    await writeFs(path, JSON.stringify([...readFile]));
-    return res.status(204).json();
+    try {
+        const { id } = req.params;
+        const readFile = await readFs();
+        const index = readFile.findIndex((element) => element.id === Number(id));
+        readFile.splice(index, 1);
+        await writeFs(path, JSON.stringify([...readFile]));
+        return res.status(204).json();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
